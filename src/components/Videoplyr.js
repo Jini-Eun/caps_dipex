822068d9d06b9c31e25e2bca56d591dc4137ef2a
EunJin <julietjin94@gmail.com>
2019-11-06 18:47:47 +0900

    modify Videoplyr.js

--- src/components/Videoplyr.js
+++ src/components/Videoplyr.js
@@ -3,71 +3,30 @@ import React, { Component } from "react";
 
 class video extends Component {
   render() {
-    //const player = new Plyr('#player');
     return (
-      <table width="90%" border="0" cellspacing="0" cellpadding="0">
+      <table width="100%" border="0" cellspacing="10" cellpadding="10">
+        <thead height="10%">
+            <tr>
+                <th colspan="2" align="left">Health4U</th>
+            </tr>
+        </thead>
         <tbody>
-          <tr>
-            <td bgcolor="#EBEBEB">
-              <table width="100%" border="0" cellspacing="1" cellpadding="0">
-                <tbody>
-                  <tr>
-                    <td height="57" bgcolor="#EBEBEB">
-                      &nbsp;
-                      <table
-                        width="95%"
-                        border="0"
-                        align="center"
-                        cellpadding="0"
-                        cellspacing="0"
-                      >
-                        <tbody>
-                          <tr>
-                            <td height="32" align="left">
-                              <strong class="strong">[성별]</strong>{" "}
-                              <span class="main">홍길동</span>
-                              <br />
-                              <strong class="strong">
-                                [인터뷰 당시 연령]
-                              </strong>{" "}
-                              <span class="main">50세</span>
-                              <br />
-                              <strong class="strong">
-                                [진단 시 연령]
-                              </strong>{" "}
-                              <span class="main">40세</span>
-                              <br />
-                            </td>
-                          </tr>
-                          <tr>
-                            <td align="left">
-                              <p class="maintop">Subtitle</p>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <br />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <br />
-              <br />
-              <table
-                width="95%"
-                border="0"
-                align="center"
-                cellpadding="0"
-                cellspacing="0"
-              >
-                <tbody>
-                  <tr>
-                    <td width="30%">
-                      <link
+        <tr bgcolor="#EBEBEB">
+                <td colspan="2">
+                    <strong class="strong">[성별]</strong>{" "}
+                    <span class="main">홍길동</span>
+                    <br />
+                    <strong class="strong">[인터뷰 당시 연령]</strong>{" "}
+                    <span class="main">50세</span>
+                    <br />
+                    <strong class="strong">[진단 시 연령]</strong>{" "}
+                    <span class="main">40세</span>
+                    <br />
+                </td>
+            </tr>
+            <tr height="85%">
+                <td width="50%">for video player<br /><br />
+                <link
                         rel="stylesheet"
                         href=" https://cdn.plyr.io/3.5.6/plyr.css "
                       />
@@ -96,45 +55,17 @@ class video extends Component {
                           default
                         />
                       </video>
-                    </td>
-                    <td width="5%" align="left" class="main">
-                      &nbsp;
-                    </td>
-                    <td width="65%" align="center" valign="top" class="main">
-                      <div id="video-footer">
-                        <strong>[Script about Video Player]</strong>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-          <br />
-          <br />
-          <tr>
-            <td>
-              <table
-                width="95%"
-                border="0"
-                align="center"
-                cellpadding="0"
-                cellspacing="0"
-              >
-                <td width="50%" align="center">
-                  Powered by
-                  <a href="https://plyr.io/" target="_blank">
-                    <strong> {this.props.name} </strong>
-                  </a>
                 </td>
-              </table>
-            </td>
-          </tr>
-          <br />
-          <br />
-          <br />
-          <br />
+                <td width="50%"><strong>[Script about Video Player]</strong><br /></td>
+            </tr>
         </tbody>
+        <tfoot height="5%" width="100%" position="static" bottom="0" align="center">
+            <tr>
+                <th colspan="2">powered by <a href="https://plyr.io/" target="_blank">
+                        <strong> {this.props.name} </strong>
+                    </a></th>
+            </tr>
+        </tfoot>
       </table>
     );
   }
