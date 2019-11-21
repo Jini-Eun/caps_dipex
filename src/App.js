f4b5b8319d7897011e848e8e97d568791d0bba2b
Changyeon Jo <jo9214@gmail.com>
2019-11-21 15:29:28 +0900

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Menu from './components/Menu.js';
+import Submenu from './components/Submenu.js';
 import Videoplyr from './components/Videoplyr.js';
 
 class App extends Component {
   render(){
     return (
       <div>
-        {/*<h1>App test </h1>*/}
-        <Videoplyr name="Plyr"/>
+        <Menu error="Unable to mount image"/>
+        <Submenu interview="인터뷰" full_list="전체 목록" age="연령별" interview_list="인터뷰 목록"/>
+        <Videoplyr gender="[성별]" interview_age="[인터뷰 당시 연령]" diagnosis_age="[진단 시 연령]" video="Plyr"/>
       </div>
       
     );
